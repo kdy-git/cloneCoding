3663ed5549b34daba7cb02c9d5e2f76fbf9f5a9d
kdy-git <86644517+kdy-git@users.noreply.github.com>
2022-08-19 11:30:29 +0900

    Add files via upload

---
+++ cloneCoding/src/test/java/com/sparta/cloneCoding/CloneCodingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sparta.cloneCoding;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CloneCodingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
