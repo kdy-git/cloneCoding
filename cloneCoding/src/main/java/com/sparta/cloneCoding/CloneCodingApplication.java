be14088b91c2d237a3272b696584f53bc2a2e519
kdy-git <zinee2001@naver.com>
2022-08-19 16:57:53 +0900

    test

--- cloneCoding/src/main/java/com/sparta/cloneCoding/CloneCodingApplication.java
+++ cloneCoding/src/main/java/com/sparta/cloneCoding/CloneCodingApplication.java
@@ -8,6 +8,7 @@ public class CloneCodingApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(CloneCodingApplication.class, args);
+		System.out.println("235523235");
 	}
 
 }
