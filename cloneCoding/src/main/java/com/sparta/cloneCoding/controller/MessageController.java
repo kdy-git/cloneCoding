835c852c1a8d40b996221011a7e2d7281aca7440
kdy-git <zinee2001@naver.com>
2022-08-22 09:46:40 +0900

    add @SendTo anotation

--- cloneCoding/src/main/java/com/sparta/cloneCoding/controller/MessageController.java
+++ cloneCoding/src/main/java/com/sparta/cloneCoding/controller/MessageController.java
@@ -6,6 +6,7 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.messaging.handler.annotation.DestinationVariable;
 import org.springframework.messaging.handler.annotation.MessageMapping;
+import org.springframework.messaging.handler.annotation.SendTo;
 import org.springframework.stereotype.Controller;
 import org.springframework.messaging.simp.SimpMessagingTemplate;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -14,7 +15,6 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
-@RequestMapping("/chat")
 @RequiredArgsConstructor
 public class MessageController {
 
@@ -23,14 +23,15 @@ public class MessageController {
 
     //채팅방 입장.
     @ResponseBody
-    @GetMapping("/{channel_Id}")
+    @GetMapping("/entry/{channel_Id}")
     public ResponseEntity<?> chatRoomEntry(@PathVariable Long channel_Id) {
 
         return ResponseEntity.ok().body(messageService.messages(channel_Id));
     }
 
     //메세지 작성
-    @MessageMapping(value = {"/message/{channel_Id}"})
+    @MessageMapping("/message/{channel_Id}")
+    @SendTo("/sub/message/{channel_Id}")
     public void chatting(MessageDto messageDto, @DestinationVariable Long channel_Id) {
 
         messageDto = messageService.sendMessage(messageDto, channel_Id);
